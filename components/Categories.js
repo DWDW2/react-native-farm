94897fd6368b24fa7ffac17aac8eefb1a2c9a1b5
DWDW2 <115546652+DWDW2@users.noreply.github.com>
2024-05-12 00:07:05 +0500

    categories added

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Image} from 'react-native';
 
+const items = [
+    {
+      image: require("../assets/images/shopping-bag.png"),
+      text: "Pick-up",
+    },
+    {
+      image: require("../assets/images/soft-drink.png"),
+      text: "Soft Drinks",
+    },
+    {
+      image: require("../assets/images/bread.png"),
+      text: "Bakery Items",
+    },
+    {
+      image: require("../assets/images/fast-food.png"),
+      text: "Fast Foods",
+    },
+    {
+      image: require("../assets/images/deals.png"),
+      text: "Deals",
+    },
+    {
+      image: require("../assets/images/coffee.png"),
+      text: "Coffee & Tea",
+    },
+    {
+      image: require("../assets/images/desserts.png"),
+      text: "Desserts",
+    },
+  ];
+
+  
 const Categories = () => {
     return (
-        <View>
-            <Text>CATE</Text>
-        </View>
+        <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+        <ScrollView horizontal showsVerticalScrollIndicator={false}> 
+        {items.map((item, index) => (
+            <View key={index} style={{alignItems:"center", marginRight:30}}>
+                <Image source={item.image} style={{width:30, height:40, resizeMode:'contain'}} />
+                <Text style={{ fontSize:13, fontWeight:'900'}}>{item.text}</Text>
+            </View>))}
+        </ScrollView>
+    </View>
     );
 }
 
